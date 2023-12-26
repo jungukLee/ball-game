66ed6286b05c2eb702b7937985967f2e4f77d309
이정욱 <jeongwook@ijeong-ug-ui-MacBookPro.local>
2023-12-26 21:20:36 +0900

    ball-games

---
+++ balls.js
@@ -0,0 +1,49 @@
+const BALLS = [
+    {
+      name: "0minigolf_ball",
+      radius: 57 / 2,
+    },
+    {
+      name: "1pool_ball",
+      radius: 70 / 2,
+    },
+    {
+      name: "2yellow_baseball_ball",
+      radius: 80 / 2,
+    },
+    {
+      name: "3tennis_ball",
+      radius: 80 / 2,
+    },
+    {
+      name: "4blue_bowling_ball",
+      radius: 127 / 2,
+    },
+    {
+      name: "5red_bowling_ball",
+      radius: 127 / 2,
+    },
+    {
+      name: "6purple_bowling_ball",
+      radius: 127 / 2,
+    },
+    {
+      name: "7voleyball_ball",
+      radius: 143 / 2,
+    },
+    {
+      name: "8basket_ball",
+      radius: 143 / 2,
+    },
+    {
+      name: "9futbol_ball",
+      radius: 143 / 2,
+    },
+    {
+      name: "10beach_ball",
+      radius: 244 / 2,
+    },
+  ];
+
+  
+  export { BALLS };
